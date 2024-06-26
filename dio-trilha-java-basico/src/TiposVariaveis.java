72150d4ca30518648cd027e115f652cac59af7d2
LucasGaldino-S <galdinosaraiva@gmail.com>
2024-06-26 20:37:58 -0300

    Testando variaveis

--- dio-trilha-java-basico/src/TiposVariaveis.java
+++ dio-trilha-java-basico/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        final double PI = 3.14;
+
+        System.out.println(PI);
     }
 }
+
+/*
+Os oito tipos primitivos em Java são:
+
+int
+byte
+short
+long 
+float
+double
+boolean
+char
+esses tipos não são considerados objetos, e portanto representam valores brutos.
+Eles são armazenados diretamente na pilha de memória. (Memory stack)
+*/
