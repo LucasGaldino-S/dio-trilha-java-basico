e8e01c0137fa370a086e8874af63f5ef743478f9
LucasGaldino-S <galdinosaraiva@gmail.com>
2024-06-27 19:26:25 -0300

    Operadores Java

---
+++ dio-trilha-java-basico/src/Operadores.java
@@ -0,0 +1,41 @@
+public class Operadores {
+    public static void main(String[] args) {
+
+        /*
+        Operadores Aritméticos
++ (Adição)
+- (Subtração)
+* (Multiplicação)
+/ (Divisão)
+% (Módulo)
+Operadores de Incremento e Decremento
+++ (Incremento)
+-- (Decremento)
+Operadores de Atribuição
+= (Atribuição)
++= (Atribuição com Adição)
+-= (Atribuição com Subtração)
+*= (Atribuição com Multiplicação)
+/= (Atribuição com Divisão)
+%= (Atribuição com Módulo)
+&= (Atribuição com AND bit-a-bit)
+|= (Atribuição com OR bit-a-bit)
+^= (Atribuição com XOR bit-a-bit)
+<<= (Atribuição com Deslocamento à Esquerda)
+>>= (Atribuição com Deslocamento à Direita)
+>>>= (Atribuição com Deslocamento à Direita sem Sinal)
+Operadores Relacionais
+== (Igual a)
+!= (Diferente de)
+> (Maior que)
+< (Menor que)
+>= (Maior ou igual a)
+<= (Menor ou igual a)
+Operadores Lógicos
+&& (AND lógico)
+|| (OR lógico)
+! (NOT lógico)
+ */
+        
+    }
+}
